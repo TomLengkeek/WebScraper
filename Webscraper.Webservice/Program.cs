effa1dee72b76e760ffaff29381c56a186f470e4
Tom Lengkeek <t.lengkeek@resoftware.nl>
2024-04-08 12:15:48 +0200

    progress

--- Webscraper.Webservice/Program.cs
+++ Webscraper.Webservice/Program.cs
@@ -1,7 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices.JavaScript;
-using System.Security.Cryptography;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using WebScraper.Webservice.Models;
 using WebScraper.Webservice.Services;
@@ -10,32 +8,53 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
 
-var limit = configuration["SearchLimit"] ?? throw new Exception("SearchLimit should be present in appsettings.json");
-var scope = configuration["SearchScope"] ?? throw new Exception("SearchScope should be present in appsettings.json");
-var fields = configuration["SearchFields"] ?? throw new Exception("SearchFields should be present in appsettings.json");
-
-//If StartDate is not present or is null take the current date as the start date
-var startData = string.IsNullOrEmpty(configuration["StartDate"])
-    ? DateTime.UtcNow
-    : DateTime.Parse(configuration["StartDate"]);
-
-
 var searchService = new SearchService(configuration);
 
-var searchRequest = new SearchRequest
+try
 {
-    Query = $"publication-date=20231215 SORT BY publication-number DESC",
-    Page = 1,
-    Limit = limit,
-    Scope = scope,
-    OnlyLatestVersions = true,
-    Fields = fields,
-};
-
-var result = await searchService.SearchAsync(searchRequest);
-
-if(string.IsNullOrEmpty(result))
-    Console.WriteLine("Error: search request did not return anything");
+    var limit = configuration["SearchLimit"] ?? throw new Exception("SearchLimit should be present in appsettings.json");
+    var scope = configuration["SearchScope"] ?? throw new Exception("SearchScope should be present in appsettings.json");
     
-var JsonObject = JToken.Parse(result);
+    var fields = configuration.GetSection("SearchFields").Get<List<Fields>>();
+
+    if (fields == null || fields.Count <= 0)
+    {
+        throw new Exception("SearchFields should be present in appsettings.json");
+    }
+    
+    var requestFields = fields.Select((f) => f.RequestName).ToArray();
+
+    var page = 0;
+    //Main loop of the webscraper
+    while (true)
+    {
+        //If StartDate is not present or is null take the current date as the start date
+        var startDate = string.IsNullOrEmpty(configuration["StartDate"])
+            ? DateTime.UtcNow
+            : DateTime.Parse(configuration["StartDate"]);
+    
+        var searchRequest = new SearchRequest
+        {
+            Query = $"publication-date={startDate.Year}{startDate.Month}{startDate.Date} SORT BY publication-number DESC",
+            Page = 1,
+            Limit = int.Parse(limit),
+            Scope = scope,
+            OnlyLatestVersions = true,
+            Fields = requestFields.ToArray(),
+        };
+
+        var result = await searchService.SearchAsync(searchRequest);
+
+        if(string.IsNullOrEmpty(result))
+            Console.WriteLine("Error: search request did not return anything");
+    
+        var JsonObject = JToken.Parse(result);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Fatal Error: {e.Message}");
+}
+
+
 
